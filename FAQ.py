5d85e0aeefee89b3bd5e7ff398e33aac8dbaf7eb
Quera <70485959+Quera-fr@users.noreply.github.com>
2024-10-03 16:31:16 +0200

    Update FAQ.py

--- FAQ.py
+++ FAQ.py
@@ -3,14 +3,9 @@ import streamlit as st
 
 st.set_page_config(
     page_title="FAQ",
-    page_icon="🧊",
+    page_icon="🔍",
     layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    initial_sidebar_state="expanded"
 )
 
 st.title("FAQ")
