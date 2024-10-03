155fe1cfd227785986e055752dfad3f9893c4277
Quera <70485959+Quera-fr@users.noreply.github.com>
2024-10-03 16:24:09 +0200

    Update FAQ.py

--- FAQ.py
+++ FAQ.py
@@ -1,24 +1,25 @@
 import streamlit as st
 
-st.title("Echo Bot")
+st.title("FAQ")
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-st.write(st.session_state.messages)
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
+if prompt := st.chat_input("Comment puis-je vous aider ?"):
+
     st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+    # Appel de l'API
     response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
+    
+    
     with st.chat_message("assistant"):
         st.markdown(response)
-    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
